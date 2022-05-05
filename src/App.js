b8b24fca9d6ee478847d209e07e61d7a914e90cc
unknown <matheus.santinello@gmail.com>
2022-05-05 01:30:50 -0300

    Implented the Fetch API functionality with success, managed to automatically get the data for the Capitals Array on Load and make it show as a list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import { Container, Global } from "./styles";
 import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-
-
+import { useEffect, useState } from "react";
 
 function App() {
+  const capitals = ['Belém' , 'Belo Horizonte', 'Brasília', 'Curitiba', 'Fortaleza', 'João Pessoa','Manaus','Rio de Janeiro', 'Salvador','São Paulo'];
+  const [capitalData, setCapitalData] = useState([]);
+  const [doneFetching, setDoneFetching] = useState(false)
+  
+  const FetchData = async(city)=>{
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=a5fb40245dd54ebe85b14651220305&q=${city}&days=1&aqi=no&alerts=no`
+    );
+    const json = await response.json();
+    console.log(json.location.name);
+    const cityData = {name: city, mintemp: json.forecast.forecastday[0].day.mintemp_c, maxtemp:json.forecast.forecastday[0].day.maxtemp_c};
+    setCapitalData(oldData=>[...oldData,cityData]);
+  }
+
+  useEffect(()=>{
+    capitals.map(i=>{
+      FetchData(i)
+    });
+    setDoneFetching(true)
+  },[]);
+
+  useEffect(()=>{
+    doneFetching && console.log(capitalData)
+  },[doneFetching])
   return (
     <>
       <Global/>
@@ -14,6 +37,13 @@ function App() {
           <input placeholder="Insira aqui o nome da cidade"></input>
           <button><FontAwesomeIcon icon={faMagnifyingGlass}/></button>
         </div>
+
+        <h2> Capitais</h2>
+        <ul>
+          {capitalData.map(i=>{
+            return <li key={i.name}>{Math.round(i.mintemp)}° {Math.round(i.maxtemp)}° {i.name}</li>
+          })}
+        </ul>
         
       </Container>
     </>
--- src/styles.js
+++ src/styles.js
@@ -18,23 +18,53 @@ export const Container = styled.main`
     flex-direction: column;
     align-items: center;
     >h1{
-        margin: 0;
         padding-bottom: 6vh;
+        margin: 0;
         margin-top: 6vh;
         color: white;
+        font-size: 4rem;
     }
     >div{
+        padding: 2rem 2rem 4rem 2rem;
+        border-color: transparent transparent white transparent ;
+        border-width: 2px;
+        border-style: solid;
         >input{
-            width:20rem;
+            height: 2rem;
+            width:32rem;
             padding: 0.5rem;
-            margin-right: -2rem;
         }
         >button{
             padding: 0;
             background-color: transparent;
             border: none;
+            position: relative;
+            right: 2rem;
             cursor: pointer;
-
+        }
+    }
+    >h2{
+        color: white;
+        position: relative;
+        right: 8rem;
+        font-size: 2.5rem;
+    }
+    >ul{
+        list-style-type: none;
+        max-width: 60rem;
+        width: 32rem;
+        max-height: 16rem;
+        text-indent: 0;
+        display: flex;
+        align-self: center;
+        flex-direction: column;
+        justify-content: center;
+        flex-wrap: wrap;
+        gap: 40px;
+        margin:0;
+        padding: 0;
+        >li{
+            color: #000000;
         }
     }
 `
